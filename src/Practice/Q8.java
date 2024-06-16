b1fc8c1c4f4d683c74c42446ccd90ef63d64df07
Lee JiEun <gsilver190@gmail.com>
2024-06-16 18:03:11 +0900

    연습 문제 8

---
+++ src/Practice/Q8.java
@@ -0,0 +1,30 @@
+package Practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q8 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("점수를 입력하세요. ");
+        int input = Integer.parseInt(br.readLine());
+
+        if (100 >= input && input >= 90) {
+            System.out.println("A");
+        }
+        else if (90 > input && input >= 80) {
+            System.out.println("B");
+        }
+        else if (80 > input && input >= 70) {
+            System.out.println("C");
+        }
+        else if (70 > input && input >= 60) {
+            System.out.println("D");
+        }
+        else {
+            System.out.println("F");
+        }
+    }
+}
